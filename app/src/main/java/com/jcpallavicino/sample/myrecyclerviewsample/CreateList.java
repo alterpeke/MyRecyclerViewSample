5f24d91570e674178cdb270c484ef4d3b2dfc646
alterpeke <elpekejuan@gmail.com>
2017-09-26 18:21:40 -0300

    Album Gallery using RecyclerView

---
+++ app/src/main/java/com/jcpallavicino/sample/myrecyclerviewsample/CreateList.java
@@ -0,0 +1,27 @@
+package com.jcpallavicino.sample.myrecyclerviewsample;
+
+/**
+ * Created by juan.pallavicino on 26/9/2017.
+ */
+
+public class CreateList {
+    private String image_title;
+    //private Integer image_id;
+    private String image_id;
+
+    public String getImage_title() {
+        return image_title;
+    }
+
+    public void setImage_title(String android_version_name) {
+        this.image_title = android_version_name;
+    }
+
+    public String getImage_ID() {
+        return image_id;
+    }
+
+    public void setImage_ID(String android_image_url) {
+        this.image_id = android_image_url;
+    }
+}
---
+++ app/src/main/java/com/jcpallavicino/sample/myrecyclerviewsample/MyAdapter.java
@@ -0,0 +1,68 @@
+package com.jcpallavicino.sample.myrecyclerviewsample;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by juan.pallavicino on 26/9/2017.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private ArrayList<CreateList> galleryList;
+    private Context context;
+
+    public MyAdapter(Context context, ArrayList<CreateList> galleryList) {
+        this.galleryList = galleryList;
+        this.context = context;
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cell_layout, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyAdapter.ViewHolder viewHolder, int i) {
+
+        Picasso.with(context)
+         .load(galleryList.get(i)
+         .getImage_ID())
+         //.resize(240, 120)
+         .into(viewHolder.img);
+        viewHolder.img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,"Image Details", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return galleryList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView title;
+        private ImageView img;
+        public ViewHolder(View view) {
+            super(view);
+
+            title = (TextView)view.findViewById(R.id.title);
+            img = (ImageView) view.findViewById(R.id.img);
+        }
+
+
+    }
+}
